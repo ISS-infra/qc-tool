9c29f40ec062213ee89a018d341d959c1283449b
Watcharin Phoemphon <83498693+DomWatcharin@users.noreply.github.com>
2024-04-23 13:34:09 +0700

    update

--- install.sh
+++ install.sh
@@ -1,19 +1,15 @@
-#!/bin/bash
-
 # Install Python 3.9 if not already installed
 if ! command -v python3.9 &> /dev/null; then
-    echo "Python 3.9 is not installed. Installing..."
-    sudo apt update
-    sudo apt install python3.9
+    echo "Python 3.9 is not installed. Please download and install Python 3.9 from https://www.python.org/downloads/"
+    exit 1
 fi
 
 # Install pip for Python 3.9
-sudo apt install python3.9-distutils
-wget https://bootstrap.pypa.io/get-pip.py
-sudo python3.9 get-pip.py
-rm get-pip.py
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3.9 get-pip.py
+del get-pip.py
 
 # Install libraries from requirements.txt
-sudo python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 echo "Installation complete."
